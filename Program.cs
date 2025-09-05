413b0acc5fa01198e18754038da23d5e95f2ccd5
Denys <fryzkodenis@gmail.com>
2025-09-05 20:42:25 +0300

    Лабораторна робота № 1
    Тема роботи: Аналіз помилок у програмному коді.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 3;
+            if (a > b)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.WriteLine("a більше");
+            }
+        }
+    }
+}
